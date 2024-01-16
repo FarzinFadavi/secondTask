7ec1d749b2db31b15996a0337153f2cd1fec4df0
ffa <falsh.tester@gmail.com>
2024-01-16 16:22:27 +0330

    Initial Commit

---
+++ secondTask/secondTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondTaskApp.swift
+//  secondTask
+//
+//  Created by Farzin macbook on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct secondTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
